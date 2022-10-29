5a79fa8ce59162d346e3dea08d73d4cd13007125
zarifjan-baltayev <zrfcnb@gmail.com>
2022-10-29 12:09:32 +0300

    version-02

---
+++ Lesson-7-Loops.py
@@ -0,0 +1,21 @@
+
+print("******")
+
+for x in range(0,100):
+    print("*******")
+    print("-----------")
+
+print("------End of The Loop!--------")
+
+for x in range (0, -31, -5):
+    print(x)
+print("----End of Minus proportion-----")
+
+x = 0
+while True:
+    print(x)
+    x = x + 1
+    if x>100:
+        break
+print("------The End-----")
+
